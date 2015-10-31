184965d1087c27bf67d33a878685772cc4a62fa2
nut-hatch <niklas.kolbe@gmail.com>
2015-10-31 16:41:36 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 sudo rm -rf ~/mas-ddos
-git clone https://nut-hatch:hall0dud4@github.com/nut-hatch/mas-ddos.git
+git clone https://nut-hatch@github.com/nut-hatch/mas-ddos.git
 cd ~/mas-ddos
 ./run.sh $@
 
